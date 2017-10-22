eab1f1d2944d5ef65e89beddf6bfd019643bd51a
RustyFox <fiona.sequeira891@gmail.com>
2017-10-22 12:59:16 -0400

    Updating README and adding webapp

---
+++ app/navbar.php
@@ -0,0 +1,40 @@
+<a href="home.php" color="#FFCA28"><h4> TweetMap </h4></a>
+    <form class="search" action="search_tags.php" align="left" method="post">       
+      <div class="searchbox">
+        <select id="hashtag" name="hashtag">                      
+          <option value="0">Search BY #tags</option>
+          <option value="#NewYork">#NewYork</option>
+          <option value="#photo">#photo</option>
+          <option value="#memories">#memories</option>
+          <option value="#parties">#parties</option>
+          <option value="#birthday">#birthday</option>
+          <option value="#metoo">#metoo</option>
+          <option value="#trump">#trump</option>
+          <option value="#India">#india</option>
+          <option value="#earth">#earth</option>
+          <option value="#tattoo">#tattoo</option>
+        </select>
+        <input type="image" alt="Submit" align="center" src="img_submit.png" name="searchbutton"  width="30" height="30"></div><br>
+        <br>
+      </div>
+    </form> 
+
+<form class="search" action="search_location.php" align="left" method="post">       
+      <div class="searchbox">
+        <select id="hashtag" name="locationtag">                      
+          <option value="0">Search BY location</option>
+          <option value="US">United States</option>
+          <option value="IN">India</option>
+          <option value="JP">Japan</option>
+          <option value="ZA">South Africa</option>
+          <option value="PH">Phillipines</option>
+          <option value="MY">Malaysia</option>
+          <option value="RO">Romania</option>
+          <option value="CA">Canada</option>
+          <option value="GB">United Kingdom</option>
+        </select>
+        <input type="image" alt="Submit" align="center" src="img_submit.png" name="searchbutton"  width="30" height="30"></div><br>
+        <br>
+      </div>
+    </form> 
+
